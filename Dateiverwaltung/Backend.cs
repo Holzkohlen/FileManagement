61cc04c96962e9f6a149dc8890592f7cb999c366
Unknown <hempel.lukas@gmail.com>
2017-05-21 14:46:03 +0200

    Some error handling for missing xml files

--- Dateiverwaltung/Backend.cs
+++ Dateiverwaltung/Backend.cs
@@ -29,12 +29,12 @@ namespace Dateiverwaltung
                 xml = new XML_IO();
                 ds_Customers = xml.readCustomers();
                 ds_Media = xml.readMedia();
-                xml.readCustomers(out customers, ref iCustomerCounter);
+                xml.readCustomers(ref customers, ref iCustomerCounter);
                 //customerList = new List<Customer>();
-                customerList = customers.ToList<Customer>();
+                if (customers != null) { customerList = customers.ToList<Customer>(); }
                 //ds_CustomersAuslesen();
                 mediaList = new List<Media>();
-                ds_MediaAuslesen();
+                if (ds_Media != null) { ds_MediaAuslesen(); }
             }
             catch (IOException e)
             {
@@ -61,36 +61,47 @@ namespace Dateiverwaltung
         private void ds_MediaAuslesen() //Liest ds_Media aus und erstellt enthaltene Objekte, speichert diese in Liste
         {
             //ds_Media.Tables[x] 0 => Media, 1 => Books, 2 => EBook, 3 => CD, 4 => DVD, 5 => BluRay
-            iMediaCounter = Int32.Parse(ds_Media.Tables[0].Rows[0][0].ToString());
-            for (int i = 1; i < ds_Media.Tables.Count; i++) //Looped durch die verschiedenen Tabellen
+            try
             {
-                for (int j = 0; j < ds_Media.Tables[i].Rows.Count; j++) //Looped durch alle Reihen einer Tabelle
+                iMediaCounter = Int32.Parse(ds_Media.Tables[0].Rows[0][0].ToString());
+                for (int i = 1; i < ds_Media.Tables.Count; i++) //Looped durch die verschiedenen Tabellen
                 {
-                    DataRow dr = ds_Media.Tables[i].Rows[j];
-                    switch (i)
+                    for (int j = 0; j < ds_Media.Tables[i].Rows.Count; j++) //Looped durch alle Reihen einer Tabelle
                     {
-                        case 1: //BOOK
-                            mediaList.Add(new Book(ds_Media.Tables[i].Rows[j][0].ToString(), ds_Media.Tables[i].Rows[j][1].ToString(), ds_Media.Tables[i].Rows[j][2].ToString(), ds_Media.Tables[i].Rows[j][3].ToString(), ds_Media.Tables[i].Rows[j][4].ToString(), ds_Media.Tables[i].Rows[j][5].ToString(), ds_Media.Tables[i].Rows[j][6].ToString(), ds_Media.Tables[i].Rows[j][7].ToString(), ds_Media.Tables[i].Rows[j][8].ToString()));
-                            break;
-                        case 2: //EBOOK
-                            mediaList.Add(new EBook(dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), dr[3].ToString(), dr[4].ToString(), dr[5].ToString(), dr[6].ToString(), dr[7].ToString(), dr[8].ToString()));
-                            break;
-                        case 3: //CD
-                            mediaList.Add(new CD(dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), dr[3].ToString(), dr[4].ToString(), dr[5].ToString(), dr[6].ToString(), dr[7].ToString(), dr[8].ToString()));
-                            break;
-                        case 4: //DVD
-                            mediaList.Add(new DVD(dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), dr[3].ToString(), dr[4].ToString(), dr[5].ToString(), dr[6].ToString(), dr[7].ToString(), dr[8].ToString(), dr[9].ToString()));
-                            break;
-                        case 5: //BluRay
-                            mediaList.Add(new BluRay(dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), dr[3].ToString(), dr[4].ToString(), dr[5].ToString(), dr[6].ToString(), dr[7].ToString(), dr[8].ToString(), dr[9].ToString()));
-                            break;
+                        DataRow dr = ds_Media.Tables[i].Rows[j];
+                        switch (i)
+                        {
+                            case 1: //BOOK
+                                mediaList.Add(new Book(ds_Media.Tables[i].Rows[j][0].ToString(), ds_Media.Tables[i].Rows[j][1].ToString(), ds_Media.Tables[i].Rows[j][2].ToString(), ds_Media.Tables[i].Rows[j][3].ToString(), ds_Media.Tables[i].Rows[j][4].ToString(), ds_Media.Tables[i].Rows[j][5].ToString(), ds_Media.Tables[i].Rows[j][6].ToString(), ds_Media.Tables[i].Rows[j][7].ToString(), ds_Media.Tables[i].Rows[j][8].ToString()));
+                                break;
+                            case 2: //EBOOK
+                                mediaList.Add(new EBook(dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), dr[3].ToString(), dr[4].ToString(), dr[5].ToString(), dr[6].ToString(), dr[7].ToString(), dr[8].ToString()));
+                                break;
+                            case 3: //CD
+                                mediaList.Add(new CD(dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), dr[3].ToString(), dr[4].ToString(), dr[5].ToString(), dr[6].ToString(), dr[7].ToString(), dr[8].ToString()));
+                                break;
+                            case 4: //DVD
+                                mediaList.Add(new DVD(dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), dr[3].ToString(), dr[4].ToString(), dr[5].ToString(), dr[6].ToString(), dr[7].ToString(), dr[8].ToString(), dr[9].ToString()));
+                                break;
+                            case 5: //BluRay
+                                mediaList.Add(new BluRay(dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), dr[3].ToString(), dr[4].ToString(), dr[5].ToString(), dr[6].ToString(), dr[7].ToString(), dr[8].ToString(), dr[9].ToString()));
+                                break;
+                        }
                     }
                 }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                //media.xml nicht vorhanden
             }
         }
 
         public void addCustomer(string sVorname, string sNachname, string sStrasse, string sPLZ, string sOrt) //Form übergibt an diese Methode zum Erstellen eines Kunden
         {
+            if(customerList == null)
+            {
+                customerList = new List<Customer>();
+            }
             customerList.Add(new Customer(Convert.ToString(iCustomerCounter), sVorname, sNachname, sStrasse, sPLZ, sOrt));
             iCustomerCounter++;
         }
@@ -120,7 +131,7 @@ namespace Dateiverwaltung
                 iMediaCounter++;
                 return true;
             }
-            catch(Exception e) { System.Windows.Forms.MessageBox.Show(e.ToString(), "ERROR"); return false; }
+            catch (Exception e) { System.Windows.Forms.MessageBox.Show(e.ToString(), "ERROR"); return false; }
         }
 
         public void editCustomer() //Daten eines Kunden bearbeiten
